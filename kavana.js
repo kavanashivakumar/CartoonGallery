70d34d87373b893f7dbe7cd6dc304e8305a79321
kavanashivakumar <kavanashivakumar8123@gmail.com>
2024-11-13 22:42:44 +0530

    verified

---
+++ kavana.js
@@ -0,0 +1,22 @@
+// Array of image file paths
+const images = ["img1.jpg","img2.jpg","img3.jpg" ,"img4.jpg","img5.jpg","img6.jpg","img7.jpg","img7.jpg","img8.jpg","img9.jpg","img9.jpg","img10.jpg","choota.jpg","kaa.jpg","oo.jpg","chutki.jpg"];
+let currentImageIndex = 0;
+
+function openImage(index) {
+  currentImageIndex = index - 1;
+  updateDisplayedImage();
+}
+
+function updateDisplayedImage() {
+  document.getElementById("displayedImage").src = images[currentImageIndex];
+}
+
+function nextImage() {
+  currentImageIndex = (currentImageIndex + 1) % images.length;
+  updateDisplayedImage();
+}
+
+function prevImage() {
+  currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+  updateDisplayedImage();
+}
